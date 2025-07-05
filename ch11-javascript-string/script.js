4436a692d68b683e527cdea2b6d227bd2a1b76b4
Mr. Kumar <lkt929129@gmail.com>
2025-07-05 15:35:25 +0530

    added string indexing in ch11- strings

--- ch11-javascript-string/script.js
+++ ch11-javascript-string/script.js
@@ -7,68 +7,71 @@
 
 // JavaScript string is a primitive data type that is used to work with texts. For example, const name = 'John';
 
-// Create JavaScript Strings
-
+// Create JavaScript Strings -
 // In JavaScript, strings are created by surrounding them with quotes. There are three ways you can use quotes.
 
 //     Single quotes: 'Hello'
 //     Double quotes: "Hello"
 //     Backticks: `Hello`
 
-// For example,
-
-//strings example
+// For example -
 const name = "Peter";
 const name1 = "Jack";
 const result = `The names are ${name} and ${name1}`;
-console.log(`rsult of declaring a string = ${result}`);
+document.write(`<br />result of declaring a string = ${result} <br /><br />`);
 
-// Creating Multi-line string -
-// JavaScript Multiline Strings
+
+// -----------------------------X-------------------------------
+// Creating Multi-line String -
 
 // To use a multiline string, you can either use the + operator or the \ operator. For example,
 
-// using the + operator
+// using "+" operator
 const message1 =
 	"This is a long message " +
 	"that spans across multiple lines" +
-	"in the code.";
+	"in the code using '+' operator. And is also multiline string.";
+
+document.write(message1 + `<br /><br />`);
 
 // using the \ operator
 const message2 =
-	"This is a long message \
+	`This is a long message \
 that spans across multiple lines \
-in the code.";
-console.log(
-	"This \
+in the code using "backslash" operator`;
+document.write(message2+"<br />This \
 is \
-a multi line string"
+a multi line string. <br /><br />"
 );
-
 // And for adding multi-line, use use '\n' escape character.
 
+
+// -----------------------------X-------------------------------
 // Escape characters in JavaScript-
 // Javascript uses '\' (backslash) in front as an escape character. To print quotes, using escape characters we have two options: For single quotes: \' (backslash followed by single quote) For double quotes: \” (backslash followed by double quotes)
 
 // Example  -
-console.log(
-	'This is an example of "Escape Charater" where backslash double quote is used as an escape character'
+document.write(
+	'This is an example of "Escape Charater" where backslash double quote is used as an escape character <br /><br />'
 );
 
+
+// -----------------------------X-------------------------------
 // Access String Characters
-
 // You can access the characters in a string in two ways.
-
-// One way is to treat strings as an array. For example,
+// One way is to treat strings as an array.
+// For example,
 
 let a = "hello";
-console.log(a[1]); // "e"
+document.write(a[1]); // "e"
 
 // Another way is to use the method charAt(). For example,
 
 let ab = "hello";
-console.log(`The output is = ${ab.charAt(1)}`); // "e"
+document.write(`The output is = ${ab.charAt(1)}`); // "e"
 
+
+// -----------------------------X-------------------------------
 // JavaScript Strings are immutable
 
 // In JavaScript, strings are immutable. That means the characters of a string cannot be changed. For example,
@@ -83,6 +86,8 @@ let ad = "hello";
 ad = "Hello";
 console.log(ad); // "Hello"
 
+
+// -----------------------------X-------------------------------
 // JavaScript is Case-Sensitive
 
 // JavaScript is case-sensitive. That means in JavaScript, the lowercase and uppercase letters are treated as different values. For example,
@@ -93,13 +98,30 @@ console.log(ag === bg); // false
 
 // In JavaScript, a and A are treated as different values.
 
+
+// -----------------------------X-------------------------------
+document.write(`<br /><br /> String Indexing`);
+document.write(`<br /> String Indexing tell the positing of charasters. `);
+document.write(`<br /> For example -`);
+const myName = `Mr Kumar`;
+document.write("<br /><br /> The position of Mr Kumar is given below-");
+document.write("<br /> M, r, , K, u, m, a, r");
+document.write("<br /> 0, 1, 2, 3, 4, 5, 6, 7");
+
+document.write(`"<br /> Printing the 5th index in "Mr Kumar" string-`);
+document.write(`"<br /> The 5th index in "Mr Kumar" string- `+ myName[5]+`<br/><br/>`);
+
+
+// -----------------------------X-------------------------------
 // JavaScript String Length
 
 // To find the length of a string, you can use built-in length property. For example,
 
 const ap = "hello";
-console.log(ap.length); // 5
+document.write(`Length of ` +ap+` is - `+ap.length); // 5
 
+
+// -----------------------------X-------------------------------
 // JavaScript String Objects
 
 // You can also create strings using the new keyword. For example,
@@ -115,8 +137,10 @@ console.log(typeof b1); // "object"
 
 // Note: It is recommended to avoid using string objects. Using string objects slows down the program.
 
-// Javascript String Concatination -
-// String concatenation means joing two or more stings together. Ths can be done by using '+' operator or by using concat(). For example,
+
+// -----------------------------X-------------------------------
+// Javascript String Concatenation -
+// String concatenation means joinIg two or more stings together. Ths can be done by using '+' operator or by using concat(). For example,
 
 // Using '+' operator -
 let str = "my name ";
@@ -128,6 +152,8 @@ const str3 = "Hello";
 const result2 = str3.concat(" World");
 console.log(result2); // Output: Hello World
 
+
+// -----------------------------X-------------------------------
 // String extraction using substr() method -
 
 // What is a substring() method?
