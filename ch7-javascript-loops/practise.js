7946400280a3af4e685daedf43cb69b75627a4e3
Mr. Kumar <lkt929129@gmail.com>
2025-07-22 12:17:20 +0530

    updated ch7-loops

---
+++ ch7-javascript-loops/practise.js
@@ -0,0 +1,18 @@
+"use Strict";
+
+// Program 1- Check if a number is odd or even in JavaScript.
+
+let n = Number(prompt(`Please Enter a Number =`));
+if (n % 2 == 0) {
+    console.log(`${n} is Even Number !`);
+} else {
+    console.log(`${n} is Odd Number !`);
+}
+
+// Program 2 - Print numbers 1 to 10.
+
+const num = 10;
+let i;
+for (i = 1; i <= num; i++) {
+    console.log(`Number = ${i}`);
+}
