e8f3f8ad47a16079b83095906f8db255ac06f873
Mr. Kumar <lkt929129@gmail.com>
2025-07-31 20:57:26 +0530

    updayes full folder

--- ch13-javascript-DOM-&BOM/practise.js
+++ ch13-javascript-DOM-&BOM/practise.js
@@ -1,6 +1,1 @@
 "use strict";
-// getElementById() example-
-let name = document.getElementById("name").innerHTML; // .textContent pulls hidden html like display:non of css;
-console.log(name);
-
-// getElementByName() example-
