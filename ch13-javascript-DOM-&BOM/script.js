ac7afc1dbb27f2986d23ac9a935b3bf608587b9b
Lalit Kumar <45649399+hellolalit@users.noreply.github.com>
2025-08-16 16:53:38 +0530

    test

--- ch13-javascript-DOM-&BOM/script.js
+++ ch13-javascript-DOM-&BOM/script.js
@@ -1,16 +1,15 @@
 "use strict";
 
-// content taken from "chai aur code youtube channel"
+// Javascript DOM (Document Object Model) -
 
-// Javascript DOM (Document Object Model) and BOM (Browser Object Model) -
-
-// theory will be applied later on because of time restrictions. Practical examples -
+// Practical examples -
 
 // content taken from - https://www.youtube.com/watch?v=b1JpH4dz4SQ&list=PLMMMqnAjMvAX9nCd85ePmuQVWkIbjSc5i&index=10  GeekStack youtube channel. Video name - "JavaScript DOM Full Course | Document Object Model JavaScript Complete Course"
 
-// manipulating the DOM -
-
-// Selecting Elements -
+/*
+Selecting Elements -
+Direct Selection -
+*/
 
 // getElementById() selects by ID in html. Example-
 let name = document.getElementById("name").innerHTML;
@@ -21,7 +20,13 @@ const selectedbtn = document.getElementsByName(`language`);
 console.log(`getelementbyName =` + selectedbtn);
 console.log(selectedbtn);
 
-// getElementByTagName() selects by html tag in html. Example-
+// getElementByTagName() selects by html tag in html. And returns multiple selected tags (here p tag) as the the <p> tag are selected in that html document.Example-
 const tagname = document.getElementsByTagName(`p`);
 console.log(`getelementbyTagName =`);
 console.log(tagname); // This will return multiple p tags as the the <p> are selected in that html document.
+
+// Indirect Selection -
+// parentElement -
+let child_list_item = document.querySelector("#html");
+let parent_list = child_list_item.parentElement;
+console.log("Parent Element =", parent_list);
