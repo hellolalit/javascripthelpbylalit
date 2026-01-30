5a5ca54d166874a0677954410bc5a7a95132b7b6
namokumar <lkt929129@gmail.com>
2026-01-30 23:12:18 +0530

    added logic for table selection

--- mini-projects/simple-search-v1/js/app.js
+++ mini-projects/simple-search-v1/js/app.js
@@ -2,3 +2,17 @@
 
 import { emp_data } from "./employee_data.js";
 console.log("Employee Data = ", emp_data);
+
+const table = document.querySelector(".search-table");
+console.log(table);
+
+let i,
+	j,
+	tablerow = table.querySelectorAll("tr");
+console.log(tablerow);
+
+for (i = 1; i < tablerow.length; i++) {
+	console.log(tablerow[i]);
+	let table_data = tablerow[i].getElementsByTagName("td");
+	console.log(table_data);
+}
